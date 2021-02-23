f7dbffa6c7c0948de129f523beeda1580c619a85
Atharva Mishra <atharvamishra956@gmail.com>
2021-02-23 19:59:27 +0530

    added js files to spawn clouds and obstacles

---
+++ js/spawn/spawn_obstacles.js
@@ -0,0 +1,31 @@
+function spawn_obstacles() {
+    if (frameCount % 60 === 0) {
+        var obstacle = createSprite(856, H / 2 - 2.5, 10, 40);
+        //obstacle.debug = true;
+        obstacle.velocityX = -(6 + 3 * score / 100);
+
+        //generate random obstacles
+        var rand = Math.round(random(1, 6));
+        switch (rand) {
+            case 1: obstacle.addImage(obstacle1);
+                break;
+            case 2: obstacle.addImage(obstacle2);
+                break;
+            case 3: obstacle.addImage(obstacle3);
+                break;
+            case 4: obstacle.addImage(obstacle4);
+                break;
+            case 5: obstacle.addImage(obstacle5);
+                break;
+            case 6: obstacle.addImage(obstacle6);
+                break;
+            default: break;
+        }
+
+        //assign scale and lifetime to the obstacle           
+        obstacle.scale = 0.5;
+        obstacle.lifetime = 300;
+        //add each obstacle to the group
+        obstacles_group.add(obstacle);
+    }
+}
