43d5cc272f0a9976d6691a93c8033a0c12e2b05c
Atharva Mishra <atharvamishra956@gmail.com>
2021-02-23 20:08:46 +0530

    chnaged position of sprite

--- js/sprites/load_game_over.js
+++ js/sprites/load_game_over.js
@@ -1,8 +1,8 @@
 function load_game_over() {
-    game_over = createSprite(300, 100);
+    game_over = createSprite(W/2, 100);
     game_over.addImage(game_over_image);
     
-    restart = createSprite(300, 140);
+    restart = createSprite(W/2, 140);
     restart.addImage(restart_image);
     
     game_over.scale = 0.5;
