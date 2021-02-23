b006394c0b6b72d35b700191182bdcee457b0fe9
Atharva Mishra <atharvamishra956@gmail.com>
2021-02-23 19:58:16 +0530

    removed js files

--- js/load_trex.js
+++
@@ -1,8 +0,0 @@
-function load_trex() {
-    trex_sprite = createSprite(30, H / 2, 20, 50);
-    trex_sprite.addAnimation("run", trex_anim);
-    trex_sprite.addAnimation("collided", trex_collide);
-    trex_sprite.scale = 0.5;
-    
-    trex_sprite.visible = false;
-}
