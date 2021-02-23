478bdf41598dbb255dfaf51dc67f9b34deb2afdc
Atharva Mishra <atharvamishra956@gmail.com>
2021-02-23 20:12:47 +0530

    showed html elements

--- js/reset.js
+++ js/reset.js
@@ -1,7 +1,14 @@
+/// <reference path="../libs/p5.d.ts"/>
+/// <reference path="../libs/p5.global-mode.d.ts"/>
+
 function reset() {
     game_state = START;
     game_over.visible = false;
     restart.visible = false;
+
+    title.show();
+    by.show();
+    start_button.show();
 
     obstaclesGroup.destroyEach();
     cloudsGroup.destroyEach();
