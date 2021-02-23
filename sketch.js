5fb96736f5dfaf3c31d37c3a143a0c6a0c0c86eb
Atharva Mishra <atharvamishra956@gmail.com>
2021-02-23 14:06:41 +0530

    added trex

--- sketch.js
+++ sketch.js
@@ -10,23 +10,40 @@ var game_state = START;
 // start screen
 var start_button, title, by;
 
+// play screen
+// trex
+var trex_sprite, trex_anim, trex_collide;
+var trex1, trex2, trex3, trex_c;
+
+function preload() {
+    // trex images
+    trex1 = loadImage("trex/trex1.png");
+    trex2 = loadImage("trex/trex2.png");
+    trex3 = loadImage("trex/trex3.png");
+    trex_c = loadImage("trex/trex_c.png");
+
+    // trex anim
+    trex_anim = loadAnimation(trex1, trex2, trex3);
+    trex_collide = loadAnimation(trex_c);
+}
+
 function setup() {
     createCanvas(W, H);
 
     // start screen
     title = createElement('h1');
     title.html("TRex Runner");
-    title.position(W/2-165, H/2-250);
+    title.position(W / 2 - 165, H / 2 - 250);
     title.id("title");
 
     by = createElement('h3');
     by.html("By: Atharva Mishra");
-    by.position(W/2-75, H/2-150);
+    by.position(W / 2 - 75, H / 2 - 150);
     by.id("by");
 
     start_button = createButton("Play!");
     start_button.size(100, 50);
-    start_button.position((W/2)-50, (H/2)-25);
+    start_button.position((W / 2) - 50, (H / 2) - 25);
     start_button.id("start_button");
 
     start_button.mousePressed(() => {
@@ -36,12 +53,30 @@ function setup() {
 
         game_state = PLAY;
     });
+
+    // play screen
+    // trex
+    trex_sprite = createSprite(50, 180, 20, 50);
+    trex_sprite.addAnimation("run", trex_anim);
+    trex_sprite.addAnimation("collided", trex_collide);
+    trex_sprite.scale = 0.5;
+
+    trex_sprite.visible = false;
 }
 
 function draw() {
-    if (game_state === PLAY) {
+    background(255);
+    textSize(20);
+    text("Score: " + score, 500, 50);
 
+    // display highscore
+    text("Highscore: " + localStorage["HighestScore"], 100, 50);
+
+    if (game_state === PLAY) {
+        trex_sprite.visible = true;
     } else if (game_state === END) {
 
     }
+
+    drawSprites();
 }
